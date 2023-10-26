a933f2b28f8eff78935c1cfafabcf5c4ca76ecf7
Lizarddapp <Lizarddapp@gmail.com>
2023-10-26 22:15:42 +0800

    initial commit

---
+++ backend/resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Your Laravel Vite React App</title>
+</head>
+<body>
+    <div id="app"></div>
+    <script src="{{ asset('vite-react-app/dist/main.js') }}"></script>
+</body>
+</html>
